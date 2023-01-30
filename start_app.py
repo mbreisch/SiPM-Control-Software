2b8b01fbf5daef809fc7bca9d12367dc77a96433
mbreisch <64076923+mbreisch@users.noreply.github.com>
2023-01-30 14:53:07 +0100

    1

--- start_app.py
+++ start_app.py
@@ -2,7 +2,7 @@ import subprocess
 import os,time,sys,signal,platform
 import pathlib
 
-def start_app(path=str(pathlib.Path().resolve())+"/flaskr"):
+def start_app(path="/home/pi/SiPM-Control-Software/flaskr"):
     """Starts Flask Development Server close with ctrl+c
 
     Args:
