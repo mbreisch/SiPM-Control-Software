e108e879df71f29ef28258a7304a81b5d1f7a491
mbreisch <64076923+mbreisch@users.noreply.github.com>
2024-06-11 19:40:23 +0200

    Update __init__.py

--- app/control/__init__.py
+++ app/control/__init__.py
@@ -1,12 +1,5 @@
 from flask import Blueprint, send_from_directory
 
-control_static_bp = Blueprint('control_static', __name__, static_folder='static', template_folder='templates')
-
-@control_static_bp.route('/static/js/<path:filename>')
-def serve_js(filename):
-    return send_from_directory(control_static_bp.static_folder + '/js', filename)
-
-# Register the blueprint with your Flask application
-app.register_blueprint(control_static_bp, url_prefix='/control')
+control_bp = Blueprint('control', __name__, static_folder='/home/pi/SSoft_exp/app/control/static', template_folder='/home/pi/SSoft_exp/app/control/templates')
 
 from . import routes
