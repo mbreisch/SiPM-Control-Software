d9c906cd8db9316f57d3f2a919b1ff776fcf4acc
mbreisch <64076923+mbreisch@users.noreply.github.com>
2024-06-11 21:45:16 +0200

    v0.0.7

--- app/control/__init__.py
+++ app/control/__init__.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, send_from_directory
-
-control_bp = Blueprint('control', __name__, static_folder='/home/pi/SSoft_exp/app/control/static', template_folder='/home/pi/SSoft_exp/app/control/templates')
+import os
+print(f"!!!!!!!!!!!!!!!!!!!!!!!!!{os.getcwd()}!!!!!!!!!!!!!!!!!!!!!!")
+control_bp = Blueprint('control', __name__, static_folder='static', template_folder='templates', url_prefix="")
 
 from . import routes
