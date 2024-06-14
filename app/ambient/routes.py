e44fcdeb1ebd8333f886a4663b94ebfca349ceae
mbreisch <64076923+mbreisch@users.noreply.github.com>
2024-06-14 18:42:48 +0200

    modified:   app/ambient/routes.py

--- app/ambient/routes.py
+++ app/ambient/routes.py
@@ -26,22 +26,108 @@ ambient_bp._title="Ambiemt Monitoring"
 @ambient_bp.route("/update_cooler", methods=["POST"])
 def update_cooler():
     with open(f'{ambient_bp.static_folder}/cooler.txt', 'r') as logfile:
-        MakeMonitorPlot("cooler", logfile)
+        MakeMonitorPlotCooler("cooler", logfile)
     return jsonify(success=True)
 
 @ambient_bp.route("/update_darkbox", methods=["POST"])
 def update_darkbox():
     with open(f'{ambient_bp.static_folder}/darkbox.txt', 'r') as logfile:
-        MakeMonitorPlot("darkbox", logfile)
+        MakeMonitorPlotDarkbox("darkbox", logfile)
     return jsonify(success=True)
 
 @ambient_bp.route("/update_outside", methods=["POST"])
 def update_outside():
     with open(f'{ambient_bp.static_folder}/outside.txt', 'r') as logfile:
-        MakeMonitorPlot("outside", logfile)
+        MakeMonitorPlotOutside("outside", logfile)
     return jsonify(success=True)
     
-def MakeMonitorPlot(name,logfile):
+def MakeMonitorPlotCooler(name,logfile):
+    # Read each line from the provided file object
+    timestamps = []
+    temperatures = []
+    humidities = []
+    for line in logfile:
+        # Strip any leading/trailing whitespace or newlines
+        line = line.strip()
+        
+        # Split the line into entries by the semicolon
+        timestamp,name,temp_hum = line.split(":") #["ts1,v1","ts2,v2",...]
+        temperature, humidity = temp_hum.strip("\n").split(";")
+        timestamps.append(float(timestamp))
+        temperatures.append(float(temperature))
+        humidities.append(float(humidity))
+        
+                  
+    last_100_timestamps = timestamps[-100:]
+    last_100_temperatures = temperatures[-100:]
+    last_100_humidities = humidities[-100:]
+    
+    # Plot timestamp vs value for each entry
+    fig, ax1 = plt.subplots(figsize=(1000/100,600/100), dpi=100)
+    ax1.plot(last_100_timestamps, last_100_temperatures, marker='', linestyle='-', markersize=3, label=f"Temperature")
+    ax1.set_xlabel('UNIX Timestamp in ms')
+    ax1.set_ylabel('Temperature in °C', color='k', fontsize=16)
+    ax1.tick_params(axis='y', labelcolor='k', labelsize=14)
+        
+    ax2 = ax1.twinx()
+    ax2.plot(last_100_timestamps, last_100_humidities, marker='', linestyle='-', markersize=3, label=f"Humidity")
+    ax2.set_ylabel('Humidity in %', color='b', fontsize=16)
+    ax2.tick_params(axis='y', labelcolor='b', labelsize=14)
+
+    # Combine all legends
+    lines1, labels1 = ax1.get_legend_handles_labels()
+    lines2, labels2 = ax2.get_legend_handles_labels()
+    ax1.legend(lines1 + lines2 , labels1 + labels2, loc='best', fontsize=16)
+
+    ax1.grid()
+    plt.title(f'Ambient {name}')
+    plt.savefig(f'{ambient_bp.static_folder}/Ambient_{name}.png')
+    plt.close()
+    
+def MakeMonitorPlotDarkbox(name,logfile):
+    # Read each line from the provided file object
+    timestamps = []
+    temperatures = []
+    humidities = []
+    for line in logfile:
+        # Strip any leading/trailing whitespace or newlines
+        line = line.strip()
+        
+        # Split the line into entries by the semicolon
+        timestamp,name,temp_hum = line.split(":") #["ts1,v1","ts2,v2",...]
+        temperature, humidity = temp_hum.strip("\n").split(";")
+        timestamps.append(float(timestamp))
+        temperatures.append(float(temperature))
+        humidities.append(float(humidity))
+        
+                  
+    last_100_timestamps = timestamps[-100:]
+    last_100_temperatures = temperatures[-100:]
+    last_100_humidities = humidities[-100:]
+    
+    # Plot timestamp vs value for each entry
+    fig, ax1 = plt.subplots(figsize=(1000/100,600/100), dpi=100)
+    ax1.plot(last_100_timestamps, last_100_temperatures, marker='', linestyle='-', markersize=3, label=f"Temperature")
+    ax1.set_xlabel('UNIX Timestamp in ms')
+    ax1.set_ylabel('Temperature in °C', color='k', fontsize=16)
+    ax1.tick_params(axis='y', labelcolor='k', labelsize=14)
+        
+    ax2 = ax1.twinx()
+    ax2.plot(last_100_timestamps, last_100_humidities, marker='', linestyle='-', markersize=3, label=f"Humidity")
+    ax2.set_ylabel('Humidity in %', color='b', fontsize=16)
+    ax2.tick_params(axis='y', labelcolor='b', labelsize=14)
+
+    # Combine all legends
+    lines1, labels1 = ax1.get_legend_handles_labels()
+    lines2, labels2 = ax2.get_legend_handles_labels()
+    ax1.legend(lines1 + lines2 , labels1 + labels2, loc='best', fontsize=16)
+
+    ax1.grid()
+    plt.title(f'Ambient {name}')
+    plt.savefig(f'{ambient_bp.static_folder}/Ambient_{name}.png')
+    plt.close()
+    
+def MakeMonitorPlotOutside(name,logfile):
     # Read each line from the provided file object
     timestamps = []
     temperatures = []
