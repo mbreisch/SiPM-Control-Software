aa2d4a27623d93d9b42af37bf729f59dec7c21c5
mbreisch <64076923+mbreisch@users.noreply.github.com>
2024-06-17 15:56:02 +0200

    modified:   app/ambient/static/js/script.js

--- app/ambient/static/js/script.js
+++ app/ambient/static/js/script.js
@@ -1,33 +1,7 @@
 async function reloadImg(url) {
-    try {
-        // Fetch the image to ensure it's reloaded
-        await fetch(url, { cache: 'reload', mode: 'no-cors' });
-
-        // Update all images with matching src attribute
-        document.body.querySelectorAll(`img[src='${url}']`).forEach(img => {
-            img.src = url + `?timestamp=${new Date().getTime()}`;
-        });
-
-        console.log(`Image reloaded: ${url}`);
-    } catch (error) {
-        console.error(`Failed to reload image: ${url}`, error);
-        // Handle error as needed, e.g., retry, log, or display a message
-    }
-}
-
-function refreshCooler() {
-    const imgUrl = "{{ url_for('ambient.static', filename='Ambient_cooler.png') }}";
-    reloadImg(imgUrl);
-}
-
-function refreshDarkbox() {
-    const imgUrl = "{{ url_for('ambient.static', filename='Ambient_darkbox.png') }}";
-    reloadImg(imgUrl);
-}
-
-function refreshOutside() {
-    const imgUrl = "{{ url_for('ambient.static', filename='Ambient_outside.png') }}";
-    reloadImg(imgUrl);
+    await fetch(url, { cache: 'reload', mode: 'no-cors' });
+    document.body.querySelectorAll(`img[src='${url}']`)
+      .forEach(img => img.src = url);
 }
 
 async function update_cooler() {
