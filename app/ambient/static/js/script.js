80031372a47c0de1711a3a5a358c5dc16d682906
mbreisch <64076923+mbreisch@users.noreply.github.com>
2024-06-17 15:43:54 +0200

    modified:   app/ambient/static/js/script.js 	modified:   app/ambient/templates/ambient.html

--- app/ambient/static/js/script.js
+++ app/ambient/static/js/script.js
@@ -4,7 +4,7 @@ async function reloadImg(url) {
       .forEach(img => img.src = url);
 }
 
-function update_cooler() {
+async function update_cooler() {
     console.log("Updating Cooler Ambients");
     $.ajax("update_cooler", {
         contentType: "application/json",
@@ -13,7 +13,7 @@ function update_cooler() {
     });
 }
 
-function update_darkbox(){
+async function update_darkbox(){
     console.log("Updating Darkbox Ambients");
     $.ajax("update_darkbox", {
         contentType: "application/json",
@@ -22,7 +22,7 @@ function update_darkbox(){
     });
 }
 
-function update_outside(){
+async function update_outside(){
     console.log("Updating Outside Ambients");
     $.ajax("update_outside", {
         contentType: "application/json",
