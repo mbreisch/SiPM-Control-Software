8cf6e0b1c547c10c3e0746ab1910affb49cea7f4
mbreisch <64076923+mbreisch@users.noreply.github.com>
2024-07-03 15:54:58 +0200

    modified:   app/ambient/routes.py

--- app/ambient/routes.py
+++ app/ambient/routes.py
@@ -174,14 +174,6 @@ def MakeMonitorPlotCooler(name,logfile):
     ax_cool_1.set_ylabel('Temperature in °C', color='r', fontsize=12)
     ax_cool_1.tick_params(axis='y', labelcolor='r', labelsize=10)
     ax_cool_1.yaxis.set_major_formatter(ticker.FormatStrFormatter('%0.1f'))
-    
-    # Calculate the interval between ticks
-    num_ticks = 5  # Desired number of ticks
-    interval = max(1, len(last_100_timestamps_date) // num_ticks)
-    tick_indices = range(0, len(last_100_timestamps_date), interval)
-    
-    ax_cool_1.set_xticks(tick_indices)
-    ax_cool_1.set_xticklabels([last_100_timestamps_date[i] for i in tick_indices], rotation=45)
         
     ax_cool_2 = ax_cool_1.twinx()
     ax_cool_2.plot(last_100_timestamps_date, last_100_humidities, color='b', marker='', linestyle='-', markersize=3, label=f"Humidity")
