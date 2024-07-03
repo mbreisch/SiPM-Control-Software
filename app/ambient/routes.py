f2739d990efd5d8e34503d423c6492de3a8c49f6
mbreisch <64076923+mbreisch@users.noreply.github.com>
2024-07-03 14:33:50 +0200

    Update routes.py

--- app/ambient/routes.py
+++ app/ambient/routes.py
@@ -122,17 +122,17 @@ def save_rht():
     darkbox = request.json["darkbox"]
     outside = request.json["outside"]
         
-    if last_timestamp.get("cooler") != cooler["timestamp"]:
+    if last_timestamps.get("cooler") != cooler["timestamp"]:
         with open(f'{ambient_bp.static_folder}/cooler.txt', 'a') as file:
             file.write(f"{cooler['timestamp']}:{'cooler'}:{cooler['temperature']};{cooler['humidity']}\n")
     last_timestamps["cooler"] = cooler["timestamp"]
     
-    if last_timestamp.get("darkbox") != darkbox["timestamp"]:
+    if last_timestamps.get("darkbox") != darkbox["timestamp"]:
         with open(f'{ambient_bp.static_folder}/darkbox.txt', 'a') as file:
             file.write(f"{darkbox['timestamp']}:{'darkbox'}:{darkbox['temperature']};{darkbox['humidity']}\n")
     last_timestamps["darkbox"] = darkbox["timestamp"]
     
-    if last_timestamp.get("outside") != outside["timestamp"]:
+    if last_timestamps.get("outside") != outside["timestamp"]:
         with open(f'{ambient_bp.static_folder}/outside.txt', 'a') as file:
             file.write(f"{outside['timestamp']}:{'outside'}:{outside['temperature']};{outside['humidity']}\n")
     last_timestamps["outside"] = outside["timestamp"]
