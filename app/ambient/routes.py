6dcc1f3fae60ca41fd767e941bef557835be85aa
mbreisch <64076923+mbreisch@users.noreply.github.com>
2024-07-08 17:59:20 +0200

    modified:   app/ambient/routes.py

--- app/ambient/routes.py
+++ app/ambient/routes.py
@@ -36,7 +36,7 @@ plt_settings = {"cooler" :
                 }
 last_timestamps = {"cooler":0, "darkbox":0, "outside":0}
 
-@ambient_bp.route("/fan_control", methods=["GET"])
+@ambient_bp.route("/fan_control", methods=["GET","POST"])
 def fan_control():
     return jsonify({"mode": request.json["mode"], "offTime": request.json["offTime"], "onTime": request.json["onTime"], "stateToggle": request.json["stateToggle"]})
 
