f5a61698d8cf1f6f53ff93a5b46fb4176615b915
mbreisch <64076923+mbreisch@users.noreply.github.com>
2024-07-08 17:16:13 +0200

    modified:   app/ambient/routes.py

--- app/ambient/routes.py
+++ app/ambient/routes.py
@@ -38,7 +38,7 @@ last_timestamps = {"cooler":0, "darkbox":0, "outside":0}
 
 @ambient_bp.route("/fan_control",methods=["GET","POST"])
 def fan_control():
-    return jsonify(request.json.data)
+    return jsonify(request.json["data"])
 
 @ambient_bp.route("/update_cooler", methods=["POST"])
 def update_cooler():
