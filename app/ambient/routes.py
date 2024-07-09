d2897a1ef575dfdeda801a28018ecc3b41cadcab
mbreisch <64076923+mbreisch@users.noreply.github.com>
2024-07-09 16:14:53 +0200

    modified:   app/ambient/routes.py

--- app/ambient/routes.py
+++ app/ambient/routes.py
@@ -148,6 +148,9 @@ def save_rht():
     cooler = request.json["cooler"]
     darkbox = request.json["darkbox"]
     outside = request.json["outside"]
+    print(cooler)
+    print(cooler["timestamp"])
+    print(type(cooler["timestamp"]))
         
     if last_timestamps.get("cooler") != cooler["timestamp"]:
         if cooler['timestamp']=="0" and cooler['temperature']=="0" and cooler['humidity']=="0":
